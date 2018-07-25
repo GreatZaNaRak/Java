ced61ad446b8b42bf29bac75c49c6fd77e693b4f
GreatZaNaRak <40637698+GreatZaNaRak@users.noreply.github.com>
2018-07-25 21:52:37 +0700

    Create sum.java

---
+++ jlab_exercise/ch.2 -expression/sum.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Try {
+	public static void main(String[] args) {
+		Scanner kb = new Scanner(System.in);
+		System.out.print("a = ");
+		int a = kb.nextInt();
+		System.out.print("b = ");
+		int b = kb.nextInt();
+		System.out.println("a + b = "+(a+b));
+	}
+}
